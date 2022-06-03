21a378847a2cc704c34ab8bc509456c9928f33fc
Mario Fabre <66124986+mariotafner@users.noreply.github.com>
2022-06-02 21:16:29 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+const client = new tmi.Client({
+    connection: {
+        reconnect: true
+    },
+    channels: ["#mariofabre", '#speedyy'],
+    identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_OAUTH_TOKEN
+    }
+});
+
+client.connect();
+
+client.on('message', async (channel, context, message) => {
+    const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
+    if ( !isNotBot ) return;
+
+    let f = (str) => {
+        str = str.replace(/[áàãâä]/g, 'a');
+        str = str.replace(/[éèêë]/g, 'e');
+        return str
+    }
+
+    if (f(message.toLocaleLowerCase().trim()) === 'que mario?')
+        client.say(channel, `/timeout ${context.username} 1`);
+});
